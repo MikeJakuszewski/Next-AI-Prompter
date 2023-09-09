77e32df9826a002654d9bf547ec52b1bc95e113e
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2023-09-08 22:52:45 -0400

    added form submition functionality

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -10,9 +10,31 @@ const CreatePrompt = () => {
     prompt: "",
     tag: "",
   });
+  const router = useRouter();
+  const session = useSession();
 
   const createPrompt = async (e) => {
     e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+          userId: session?.data?.user.id,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(`This is the type of error: ${error}`);
+    } finally {
+      setSubmitting(false);
+    }
   };
 
   return (
